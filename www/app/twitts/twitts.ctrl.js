f353271ffe6a344dcaeae9faf03ce486f7390cdb
Denzel Wamburu <denzjoseph@gmail.com>
2016-01-15 21:13:40 +0300

    added post function for tweets

--- www/app/twitts/twitts.ctrl.js
+++ www/app/twitts/twitts.ctrl.js
@@ -3,12 +3,14 @@
   angular.module('app')
     .controller('TwittsCtrl', TwittsCtrl);
 
-  function TwittsCtrl($scope, Storage, Backend, $ionicFilterBar, User){
+  function TwittsCtrl($scope, Storage, Backend, $ionicFilterBar, User, $state){
     var data = {}, fn = {};
     $scope.data = data;
     $scope.fn = fn;
     var filterBarInstance;
     $scope.currentUser = User.getCurrent();
+  //  $scope.tweets = [];
+    $scope.tweet = {};
 
     $scope.$on('$ionicView.enter', function(){
       Storage.getTweets().then(function(tweets){
@@ -31,5 +33,50 @@
       }
     });
   };
+  // post and save tweet
+   $scope.postTweet = function() {
+      Tweet
+        .create({
+          content: $scope.tweet.content,
+          id: $scope.tweet.id,
+          date : new Date().toJSON(),
+          ownerId : $scope.currentUser.id,
+          ownerUsername : $scope.currentUser.username
+        })
+        .$promise
+        .then(function() {
+          $state.go('app.twitts');
+        });
+    };
   }
+
+/**  .controller('AddReviewController', ['$scope', 'CoffeeShop', 'Review',
+      '$state', function($scope, CoffeeShop, Review, $state) {
+    $scope.action = 'Add';
+    $scope.coffeeShops = [];
+    $scope.selectedShop;
+    $scope.review = {};
+    $scope.isDisabled = false;
+
+    CoffeeShop
+      .find()
+      .$promise
+      .then(function(coffeeShops) {
+        $scope.coffeeShops = coffeeShops;
+        $scope.selectedShop = $scope.selectedShop || coffeeShops[0];
+      });
+
+    $scope.submitForm = function() {
+      Review
+        .create({
+          rating: $scope.review.rating,
+          comments: $scope.review.comments,
+          coffeeShopId: $scope.selectedShop.id
+        })
+        .$promise
+        .then(function() {
+          $state.go('all-reviews');
+        });
+    };
+  }]) **/
 })();
