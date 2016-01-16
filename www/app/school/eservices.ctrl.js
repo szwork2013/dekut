43cd2dd5b9b10563de08d87c63ac76e1c92e797f
Denzel Wamburu <denzjoseph@gmail.com>
2016-01-16 15:36:00 +0300

    added eservice component and controller

---
+++ www/app/school/eservices.ctrl.js
@@ -0,0 +1,27 @@
+(function(){
+'use strict';
+angular.module('app')
+  .controller('EservicesCtrl', EservicesCtrl);
+
+function EservicesCtrl($scope, User, Eservice){
+  $scope.currentUser = User.getCurrent();
+  $scope.eservice = {};
+
+  // do post
+  $scope.RequestEservice = function() {
+    Eservice
+      .create({
+        type : $scope.eservice.type,
+        content: $scope.eservice.content,
+        phonenumber : $scope.eservice.phonenumber,
+        email : $scope.eservice.email
+
+      })
+      .$promise
+      .then(function() {
+      console.log('err')
+      });
+  };
+
+}
+})();
