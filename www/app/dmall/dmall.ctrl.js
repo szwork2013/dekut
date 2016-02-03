dc2dc53a7871883bf00fcd303a613427cc12414e
Denzel Wamburu <denzjoseph@gmail.com>
2016-02-03 23:07:59 +0300

    updated products services

--- www/app/dmall/dmall.ctrl.js
+++ www/app/dmall/dmall.ctrl.js
@@ -4,6 +4,20 @@
     .controller('ProductsCtrl', ProductsCtrl);
 
 
+    function ProductsCtrl($scope, $ionicFilterBar, User, $state, Product, $location, $ionicModal, $timeout, $rootScope, $ionicUser, $ionicPush){
+
+    $scope.showFilterBar = function () {
+    filterBarInstance = $ionicFilterBar.show({
+      notices: $scope.notices,
+      update: function (filteredNotices, filterText) {
+        $scope.notices = filteredNotices;
+        if (filterText) {
+          console.log(filterText);
+        }
+      }
+    });
+  };
+
 $scope.products = Product.find({
 
 })
