f5cef8493ed27a00433d1fcdf707971ce0de080a
Denzel Wamburu <denzjoseph@gmail.com>
2016-02-09 22:51:04 +0300

    add MakeOrder Func

--- www/app/dmall/product.ctrl.js
+++ www/app/dmall/product.ctrl.js
@@ -3,7 +3,7 @@
   angular.module('app')
     .controller('ProductCtrl', ProductCtrl, ['lbServices', 'ionic', 'ProductCtrl']);
 
-  function ProductCtrl($scope, $stateParams, $ionicModal, User, Product, $location, Category){
+  function ProductCtrl($scope, $stateParams, $ionicModal, User, Product, $location, Category, Order){
 
 Product
     .find({
@@ -24,9 +24,33 @@ Product
     });
 
     $scope.currentUser = User.getCurrent();
-        $scope.product = {};
+    $scope.product = {};
+    $scope.newOrder = {};
+    // calculate total amount from quantity
+    $scope.productTotalAmount = $scope.price * $scope.productQuantity;
 
+    $scope.MakeOrder = function () {
+        $scope.close();
+        $scope.newOrder.date = new Date().toJSON();
+        $scope.newOrder.productName = $scope.productName;
+        $scope.newOrder.productQuantity = $scope.productQuantity;
+        $scope.newOrder.productTotalAmount = $scope.productTotalAmount;
+        $scope.newOrder.customerId = $scope.currentUser.id;
+        $scope.newOrder.customerName = $scope.currentUser.names;
+        $scope.newOrder.customerPhone = $scope.currentUser.phonenumber;
+        $scope.newOrder.customerEmail = $scope.currentUser.email;
+        $scope.newOrder.location = $scope.location;
+        $scope.newOrder.detail = $scope.detail;
+        Order.create($scope.newOrder,
+            function (res) {
+                delete $scope.newOrder;
+        //      do something after order
 
+            },
+            function (err) {
+                console.log(err)
+            })
+    };
 
 
   }
