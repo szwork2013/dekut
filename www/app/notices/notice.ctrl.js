86e488a119ab9ec764871d323bf50ef72fdd24de
Denzel Wamburu <denzjoseph@gmail.com>
2016-02-13 20:16:55 +0300

    social sharing

--- www/app/notices/notice.ctrl.js
+++ www/app/notices/notice.ctrl.js
@@ -3,7 +3,7 @@
   angular.module('app')
     .controller('NoticeCtrl', NoticeCtrl, ['lbServices', 'ionic', 'NoticesCtrl']);
 
-  function NoticeCtrl($scope, $stateParams, Storage, $ionicModal, User, Notice, Avatar, $location, Restangular){
+  function NoticeCtrl($scope, $stateParams, Storage, $ionicModal, User, Notice, Avatar, $location){
 
 Notice
     .find({filter: {where: {id: $stateParams.id}}})
@@ -20,7 +20,20 @@ Notice
     $scope.currentUser = User.getCurrent();
         $scope.notice = {};
 
-
+// social sharing
+$scope.shareNative = function() {
+        if (window.plugins && window.plugins.socialsharing) {
+            window.plugins.socialsharing.share("Checkout Notice in DekutApp: " + $scope.notice.title + ",By" + $scope.notice.author + ".",
+                '', null, "http://twitter.com/DekutApp",
+                function() {
+                    console.log("Success")
+                },
+                function (error) {
+                    console.log("Share fail " + error)
+                });
+        }
+        else console.log("Share plugin not available");
+}
 
 
   }
