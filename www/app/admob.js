b4d39b809f791a4ff501d2eb2782f0bdc09e550f
Denzel Wamburu <denzjoseph@gmail.com>
2016-02-24 08:18:32 +0300

    update plugins

--- www/app/admob.js
+++ www/app/admob.js
@@ -1,18 +1,18 @@
 var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) {
     admobid = { // for Android
-        banner: 'ca-app-pub-6869992474017983/9375997553',
-        interstitial: 'ca-app-pub-6869992474017983/1657046752'
+        banner: 'ca-app-pub-8427277104243864/7615819430',
+        interstitial: 'ca-app-pub-8427277104243864/9092552630'
     };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
     admobid = { // for iOS
-        banner: 'ca-app-pub-6869992474017983/4806197152',
-        interstitial: 'ca-app-pub-6869992474017983/7563979554'
+        banner: 'ca-app-pub-8427277104243864/7615819430',
+        interstitial: 'ca-app-pub-8427277104243864/9092552630'
     };
 } else {
     admobid = { // for Windows Phone
-        banner: 'ca-app-pub-6869992474017983/8878394753',
-        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+        banner: 'ca-app-pub-8427277104243864/7615819430',
+        interstitial: 'ca-app-pub-8427277104243864/9092552630'
     };
 }
 
